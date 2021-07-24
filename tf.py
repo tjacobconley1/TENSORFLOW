e0fd269f06ad3810530d414cbf29e87a0f0a6a86
Tyler Conley <tjacobconley1@gmail.com>
2021-07-24 02:18:49 -0700

    got it, had typos n such

--- tf.py
+++ tf.py
@@ -1,21 +1,22 @@
 
-import numpy
-import tensorflow
-
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
 
 # declare 'model' density and shape
 model = keras.Sequential([keras.layers.Dense(units=1, input_shape=[1])])
 # the 'optimizer' function generates a guess
 # the 'loss' function calculates the percentage difference 
 # (images compared pixel by pixel??)
-model.compile(optimizer='sgd', loss='mean_sqared_error')
+model.compile(optimizer='sgd', loss='mean_squared_error')
 
 
 # 6 possible values for x and y from
 #        y = 2x - 1
 # held in corresponding numpy arrays
 xs = np.array([-1.0, 0.0, 1.0, 2.0, 3.0, 4.0], dtype=float)
-ys = np.array([-3.0, -1.0, 1.0, 3.0, 5.0, 7.0]. dtype=float)
+ys = np.array([-3.0, -1.0, 1.0, 3.0, 5.0, 7.0], dtype=float)
 
 # check 'xs' against 'ys' 500 times using 'fit'
 # this is what trains the model
@@ -23,4 +24,4 @@ model.fit(xs, ys, epochs=500)
 
 #
 # 'model.predict([a guess value])'
-print(model.predict([10.0])
+print(model.predict([10.0]))
