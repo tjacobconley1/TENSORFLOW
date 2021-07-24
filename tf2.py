b5cd7e2d7e8a584045d6a7725698e519f16ceb55
Tyler Conley <tjacobconley1@gmail.com>
2021-07-24 03:45:09 -0700

    getting an indent error,  time to get a redbull lol

--- tf2.py
+++ tf2.py
@@ -19,7 +19,7 @@ model = tf.keras.models.Sequential([
    # across the image
    # Then each epoch it will figure out which filters gave the best
    # signals to help match the images to their labels
-    tf.keras.layers.Conv2D(64, (3,3), activation='relu', input_shape=(28,28,1))
+    tf.keras.layers.Conv2D(64, (3,3), activation='relu', input_shape=(28,28,1)),
    # compresses and enhances image
     tf.keras.layers.MaxPooling2D(2,2),
    #
@@ -29,5 +29,67 @@ model = tf.keras.models.Sequential([
     tf.keras.layers.Dense(10, activation='softmax')
 ])
 
+# directory containing training data
+TRAINING_DIR = '/tmp/faces/'
+
+#
+training_datagen = ImageDataGenerator(rescale = 1./255)
+
+#
+train_generator = training_datagen.flow_from_directory(
+    TRAINING_DIR,
+    target_size=(150,150),
+    class_mode='categorical'
+)
+
+#
+validation_generator = validation_datagen.flow_from_directory(
+    VALIDATION_DIR,
+    target_size=(150,150),
+    class_mode='categorical'
+)
+
+
+
+# defining our neural network
+model = tf.keras.models.Sequential([
+   # shape is now 150x150
+   # we now have 4 layers of convolution all with max pooling
+    tf.keras.layers.Conv2D(64, (3,3), activation='relu', input_shape=(150,150,3)),
+    tf.keras.layers.MaxPooling2D(2,2),
+
+    tf.keras.layers.Conv2D(64, (3,3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2,2),
+
+    tf.keras.layers.Conv2D(128, (3,3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2,2),
+
+    tf.keras.layers.Conv2D(128, (3,3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2,2),
+
+   # Those 4 layers are then fed into the following dense layer
+   # Flatten the results to feed into a DNN
+    tf.keras.layers.Flatten(),
+   # the dropout increases efficiency of the NN by eliminating some of the results
+    tf.keras.layers.Dropout(0.5),
+
+   # 512 neuron hidden layer
+    tf.keras.layers.Dense(512, activation='relu'),
+    tf.keras.layers.Dense(3, activation='softmax')
+
+])
+
+   # compile the NN
+    model.compile(loss='categorical_crossentropy',
+        optimizer='rmsprop',
+        metrics=['accuracy'])
+
+
+   # fit the data
+   # the training an validation datasets aren't labeled with a directory
+   # because we are generating the dataset using the generators
+    history = model.fit_generator(train_generator, epochs=25,
+        validation_data = validation_generator,
+        verbose = 1)
 
 
--- unzip.py
+++ unzip.py
@@ -6,10 +6,10 @@ import zipfile
 
 local_zip = 'faces.zip'
 zip_ref = zipfile.ZipFile(local_zip, 'r')
-zip_ref.extractall('/tmp')
+zip_ref.extractall('/.git/TENSORFLOW/tmp')
 zip_ref.close()
 
 local_zip = 'facestest.zip'
 zip_ref = zipfile.ZipFile(local_zip, 'r')
-zip_ref.extractall('/tmp/')
+zip_ref.extractall('/.git/TENSORFLOW/tmp')
 zip_ref.close()
